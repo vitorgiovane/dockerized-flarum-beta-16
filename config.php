180dd748442090359d85a2547ab72ba212358d86
Vitor Giovane <vitor.alves@pontomais.com.br>
2021-03-28 23:21:46 -0300

    feat: inicializa flarum beta 16 com o Docker

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'debug' => false,
+  'poweredByHeader' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'memore-mysql',
+    'port' => 3306,
+    'database' => 'memore',
+    'username' => 'memore',
+    'password' => 'memore',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'mem_',
+    'strict' => false,
+    'engine' => 'InnoDB',
+    'prefix_indexes' => true,
+  ),
+  'url' => 'http://localhost:8010',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
